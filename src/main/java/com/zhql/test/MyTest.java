05a02b5d73c35efa23eda778a369e7f76aa94d93
ZHENGQILIN543 <ZHENGQILIN543@pingan.com.cn>
2021-05-07 18:26:21 +0800

    second commit

--- src/main/java/com/zhql/test/MyTest.java
+++ src/main/java/com/zhql/test/MyTest.java
@@ -8,5 +8,6 @@ package com.zhql.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("version 1");
+        System.out.println("version 2");
     }
 }
