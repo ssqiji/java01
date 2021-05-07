868e463220f8536870731246e31a341c3074baaa
ZHENGQILIN543 <ZHENGQILIN543@pingan.com.cn>
2021-05-07 20:04:40 +0800

    hot-fix commit 1

--- src/main/java/com/zhql/test/MyTest.java
+++ src/main/java/com/zhql/test/MyTest.java
@@ -9,5 +9,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
+        System.out.println("hello");
     }
 }
